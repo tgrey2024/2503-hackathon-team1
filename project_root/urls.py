ce573ba5579b37f1eac12a7d62043a39394f6306
j0hanz <golfarenlj@gmail.com>
2025-03-20 16:43:40 +0100

    Initial Tailwind CSS setup and integration

--- project_root/urls.py
+++ project_root/urls.py
@@ -6,5 +6,6 @@ from django.urls import include, path
 urlpatterns = [
     path('accounts/', include('allauth.urls')),
     path('admin/', admin.site.urls),
+    path("__reload__/", include("django_browser_reload.urls")),
     path('', include('home.urls')),
 ]
