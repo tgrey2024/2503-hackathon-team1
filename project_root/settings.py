3f1a3650791438b6864c40f9e009c3d7fe96a39a
j0hanz <golfarenlj@gmail.com>
2025-03-21 22:18:05 +0100

    Add django unfold with tailwind admin panel

--- project_root/settings.py
+++ project_root/settings.py
@@ -26,6 +26,11 @@ ALLOWED_HOSTS = [
 
 # Application definition
 INSTALLED_APPS = [
+    'unfold',
+    'unfold.contrib.filters',
+    'unfold.contrib.forms',
+    'unfold.contrib.import_export',
+    'import_export',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
