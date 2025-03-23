7f2b08e6081604776e27b83cb19749d135d7b15e
j0hanz <golfarenlj@gmail.com>
2025-03-23 21:31:37 +0100

    Add event detail view and update timeline template for honour feature

--- timeline/views.py
+++ timeline/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import JsonResponse, HttpRequest, HttpResponse
 from django.contrib.auth.decorators import login_required
+from django.db.models import Count
 from .models import Timeline
 from utils.timeline import get_timeline_events, mark_user_honoured_events, toggle_event_honour
 
@@ -9,6 +10,16 @@ def timeline(request: HttpRequest) -> HttpResponse:
     events = get_timeline_events()
     events = mark_user_honoured_events(events, request.user)
     return render(request, 'timeline/timeline.html', {'events': events})
+
+def event_detail(request: HttpRequest, timeline_id: int) -> HttpResponse:
+    """Display detailed view of a timeline event"""
+    event = get_object_or_404(Timeline.objects.annotate(honour_count=Count('honours')), id=timeline_id)
+
+    events = [event]
+    events = mark_user_honoured_events(events, request.user)
+    event = events[0]
+
+    return render(request, 'timeline/honour.html', {'event': event})
 
 @login_required
 def toggle_honour(request: HttpRequest, timeline_id: int) -> JsonResponse:
