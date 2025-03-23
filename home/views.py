6120fa11d736819fd3bb0a781e6b358864a6f9fd
Swewi <kireebellamy@gmail.com>
2025-03-23 11:45:47 +0100

    home/timeline integration

--- home/views.py
+++ home/views.py
@@ -1,4 +1,11 @@
 from django.shortcuts import render
+from timeline.models import Timeline
 
 def home(request):
+    # Get ALL timeline events, sorted by year
+    timeline_events = Timeline.objects.all().order_by('-year')
+    
+    context = {
+        'timeline_events': timeline_events
+    }
     return render(request, 'home/index.html')
