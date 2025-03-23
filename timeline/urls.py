655e9977c76fb78c971270246bb846c907addd86
Swewi <kireebellamy@gmail.com>
2025-03-23 18:37:16 +0100

    added custom error html

--- timeline/urls.py
+++ timeline/urls.py
@@ -6,4 +6,5 @@ app_name = 'timeline'
 urlpatterns = [
     path('', views.timeline, name='timeline'),
     path('honour/<int:timeline_id>/', views.toggle_honour, name='toggle_honour'),
+    path('<int:timeline_id>/honour/', views.toggle_honour, name='toggle_honour'),
 ]
